f36d3293de1e2529ae6336852e061a5ebb4f42b8
Matthew Berntsen <matthewberntsen@w134-87-135-189.wireless.uvic.ca>
2018-10-10 20:51:54 -0700

    practicing keras api

--- tensorpractice.py
+++ tensorpractice.py
@@ -2,9 +2,18 @@
 
 import tensorflow as tf
 
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+
+model = Sequential()
+model.add(Dense(32, input_shape=(16,)))
+#above line adds first layer with input arrays of (*,16)
+#and outputs an array of shape (*, 32)
+model.add(Dense(32))
+
 x1 = tf.constant([1, 2, 4, 5])
 x2 = tf.constant([2, 2, 1, 4])
 result = tf.multiply(x1, x2)
-se = tf.Session()
-print(se.run(result))
-se.close()
+with tf.Session() as sesh:
+    output = sesh.run(result)
+    print(output)
